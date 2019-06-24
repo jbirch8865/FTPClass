5f90da2028f5a2ca0bb434ec4b8375c71097be6a
Joel Birch <jbirch88655@gmail.com>
2019-06-24 14:40:06 -0700

    Started class

---
+++ FTPClass.php
@@ -0,0 +1,66 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+class BadIniFile extends Exception {}
+class BadFTPLogin extends Exception {}
+
+class FTP_Link {
+	private $FTP_connection;
+	private $FTP_hostname;
+	private $FTP_username;
+	private $FTP_password;
+	private $FTP_connection_timeout;
+	private $FTP_listening_port;
+
+	function __construct() {
+		try {
+			$this->LoadIniConfigs();
+			$this->FTP_connection = ftp_connect($this->FTP_hostname,$this->FTP_listening_port,$this->FTP_connection_timeout);
+			ftp_login($this->FTP_connection,$this->FTP_username,$this->FTP_password);
+		} catch (BadIniFile $e) {
+			throw new BadIniFile("Error loading Ini configurations");
+		} catch (Exception $e) {
+			throw new BadFTPLogin("Error logging into the FTP server");
+		}
+    	}
+
+	private function LoadIniConfigs()
+	{
+		try {
+			$FTP_Configs = parse_ini_file('FTP_config.ini');
+			$this->FTP_hostname = $FTP_Configs['hostname'];
+			$this->FTP_username = $FTP_Configs['username'];
+			$this->FTP_password = $FTP_Configs['password'];
+			$this->FTP_connection_timeout = $FTP_Configs['timeoutperiod'];
+			$this->FTP_listening_port = $FTP_Configs['ftpport'];
+		} catch (Exception $e) {
+			throw new BadIniFile("Error loading the ini file configs");
+		}
+	}
+
+	private function Close_FTP_Connection()
+	{
+		try
+		{
+			ftp_close($this->FTP_connection);
+			return true;
+		} catch (Exception $e)
+		{
+			return false;
+		}
+	}
+
+	public function List_Files($Directory = '.')
+	{
+		try {
+			return ftp_nlist($this->FTP_Connection,$Directory);
+		} catch (Exception $e) {
+			throw new Exception("Error loading directory");
+		}
+	}
+}
+
+$FTP = new FTP_Link;
+print_r($FTP->List_Files());
+?>
