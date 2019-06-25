0cf96a788f7b348034c870f161c725e7a4fc0c5b
Joel Birch <jbirch88655@gmail.com>
2019-06-24 19:15:45 -0700

    Added max file size for larger file uploads

--- FTPClass.php
+++ FTPClass.php
@@ -1,6 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('upload_max_filesize', 500M);
+ini_set('post_max_size',500M);
 error_reporting(E_ALL);
 class BadIniFile extends Exception {}
 class BadFTPLogin extends Exception {}
